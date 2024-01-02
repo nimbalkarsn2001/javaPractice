0b430c38484fbdd03dd8af6e3840ac9add21aaa2
shivraj <shivrajnnimbalkar@gmail.com>
2024-01-02 22:05:08 +0530

    add file uploaded

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.until.scanner;
+class add
+{
+public static void main (string args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(system.in);
+system.out.println("enter first no:");
+a=sc.nextint();
+System.out.println("enter second no;");
+b=sc.nextint();
+c=a+b;
+system.out.println("addition="+c);
+}
+}
