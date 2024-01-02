0ac12cd20078f3d249088c8c3d7849287d3a942d
shivraj <shivrajnnimbalkar@gmail.com>
2024-01-02 21:57:47 +0530

    java file added

---
+++ Text.java
@@ -0,0 +1,16 @@
+public class Text {
+
+
+		void show(int a){
+		system.out.println("int the area");
+}		
+		void show(){
+		system.out.println("5");
+
+		
+}
+		Public static void main (String args[]){
+		Text bb=new Text(); 
+		bb.show();
+}
+}	
